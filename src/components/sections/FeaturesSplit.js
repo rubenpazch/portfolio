81c88e602c2e63d283256ae0de9db0f9dad62748
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-03-10 17:20:00 -0500

    Add  split elements

--- src/components/sections/FeaturesSplit.js
+++ src/components/sections/FeaturesSplit.js
@@ -29,17 +29,39 @@ const FeaturesSplit = ({
   ...props
 }) => {
 
-  const [videoModalActive, setVideomodalactive] = useState(false);
+  const [videoModalActive1, setVideomodalactive1] = useState(false);
+  const [videoModalActive2, setVideomodalactive2] = useState(false);
+  const [videoModalActive3, setVideomodalactive3] = useState(false);
 
-  const openModal = (e) => {
+  const openModal1 = (e) => {
     e.preventDefault();
-    setVideomodalactive(true);
+    setVideomodalactive1(true);   
   }
 
-  const closeModal = (e) => {
+  const closeModal1 = (e) => {
     e.preventDefault();
-    setVideomodalactive(false);
-  }   
+    setVideomodalactive1(false);
+  }
+
+  const openModal2 = (e) => {
+    e.preventDefault();
+    setVideomodalactive2(true);   
+  }
+
+  const closeModal2 = (e) => {
+    e.preventDefault();
+    setVideomodalactive2(false);
+  }
+
+  const openModal3 = (e) => {
+    e.preventDefault();
+    setVideomodalactive3(true);   
+  }
+
+  const closeModal3 = (e) => {
+    e.preventDefault();
+    setVideomodalactive3(false);
+  }
 
   const outerClasses = classNames(
     'features-split section',
@@ -101,7 +123,7 @@ const FeaturesSplit = ({
                     <li>API Rest</li>
                   </ul>
                 </div>
-                <Button tag="a" color="light" wideMobile href="https://cruip.com/" className="mt-32" onClick={openModal}>
+                <Button tag="a" color="light" wideMobile href="#" className="mt-32" onClick={openModal1}>
                   See Project
                 </Button>
               </div>
@@ -117,8 +139,8 @@ const FeaturesSplit = ({
               </div>
               <Modal
                 id="video-modal"
-                show={videoModalActive}
-                handleClose={closeModal}
+                show={videoModalActive1}
+                handleClose={closeModal1}
                 children={"<h1>casa</h1>"}
                 videoTag="iframe"
                 titleContent="Hospital Appointment System"
@@ -127,17 +149,16 @@ const FeaturesSplit = ({
                 yearContent="2020"
                 projectContent="Hospital Appointment System is a web application tablet version, this application was build using ROR and React & Redux the latest version, includes the ability to register new users, login into the application creating a token session, when we are logged in the application we can select a service (Surgery, Medicine and others), each of the options show different doctors and their attendance availability, finally we can create an appointment for a specific date and service."
                 featureImage="portfolio1.png"
-                techList={[{id: 1 ,name: "Ruby and Rails"}, {id: 2 ,name:"React & Redux"}, {id: 2 ,name:"Postgres"}, {id: 2 ,name:"API Rest"}]}
+                techList={[{id: 1 ,name: "Ruby and Rails"}, {id: 2 ,name:"React & Redux"}, {id: 3 ,name:"Postgres"}, {id: 4 ,name:"API Rest"}]}
                 linkGithub={"https://github.com/rubenpazch/appointment-view"}
                 linkLivePreview={"https://agile-lowlands-59460.herokuapp.com/Dashboard"}
               />  
             </div>
-            
 
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <h3 className="mb-16 p-0">
-                  Hospital Appointment System
+                  e-Tours Web Application
                 </h3>
                 <div className="p-0 mb-8 mt-0">
                   <ul className="item_list m-0 p-0">
@@ -147,17 +168,18 @@ const FeaturesSplit = ({
                   </ul>
                 </div>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua 
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua 
+                  e-Tours Web Application is a web application desktop version to receive a list of tours from an API
                 </p>
                 <div className="split-item-languages m-0 p-0">
                   <ul className="m-0 p-0">
-                    <li>Ruby and Rails</li>
-                    <li>Ruby</li>
                     <li>React</li>
+                    <li>Redux</li>
+                    <li>CSS Modules</li>
+                    <li>React Hooks</li>
+                    <li>React Testing Library</li>
                   </ul>
                 </div>
-                <Button tag="a" color="light" wideMobile href="https://cruip.com/" className="mt-32">
+                <Button tag="a" color="light" wideMobile href="#" className="mt-32" onClick={openModal3}>
                   See Project
                 </Button>
                 <div>
@@ -171,15 +193,31 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/portfolio1.png')}
+                  src={require('./../../assets/images/portfolio3.png')}
                   alt="Project Appointment"/>
               </div>
+              <Modal
+                id="video-modal"
+                show={videoModalActive3}
+                handleClose={closeModal3}
+                children={"<h1>casa</h1>"}
+                videoTag="iframe"
+                titleContent="e-Tours Web Application"
+                companyContent="Microverse"
+                roleContent="Full Stack Developer"
+                yearContent="2020"
+                projectContent="e-Tours Web Application is a web application desktop version to receive a list of tours from an API, this information is dynamic, we are able to filter our list by price, title or also we can use the search option to find tours for a specific city (New York, Mexico Cito, Lima, Boston), check tours details by clicking each tour. This application was build using React & Redux."
+                featureImage="portfolio3.png"
+                techList={[{id: 1 ,name: "React"}, {id: 2 ,name:"Redux"}, {id: 3 ,name:"CSS Modules"}, {id: 4 ,name:"React Hooks"}, {id: 5 ,name:"React Testing Library"}]}
+                linkGithub={"https://github.com/rubenpazch/catalogue-of-dog-clothes"}
+                linkLivePreview={"https://sad-sinoussi-c7723c.netlify.app/"}
+              />  
             </div>
-
+            
             <div className="split-item">
               <div className="split-item-content center-content-mobile reveal-from-left" data-reveal-container=".split-item">
                 <h3 className="mb-16 p-0">
-                  Hospital Appointment System
+                  Weather App
                 </h3>
                 <div className="p-0 mb-8 mt-0">
                   <ul className="item_list m-0 p-0">
@@ -189,17 +227,16 @@ const FeaturesSplit = ({
                   </ul>
                 </div>
                 <p className="m-0">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua 
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua 
+                This project is about building a web application to show a weather forecast using weather API from external services.
                 </p>
                 <div className="split-item-languages m-0 p-0">
                   <ul className="m-0 p-0">
-                    <li>Ruby and Rails</li>
-                    <li>Ruby</li>
-                    <li>React</li>
+                    <li>Javascript 6</li>
+                    <li>Webpack 3</li>
+                    <li>Rest API</li>
                   </ul>
                 </div>
-                <Button tag="a" color="light" wideMobile href="https://cruip.com/" className="mt-32">
+                <Button tag="a" color="light" wideMobile href="#" className="mt-32" onClick={openModal2}>
                   See Project
                 </Button>
                 <div>
@@ -213,9 +250,25 @@ const FeaturesSplit = ({
                 )}
                 data-reveal-container=".split-item">
                 <Image
-                  src={require('./../../assets/images/portfolio1.png')}
+                  src={require('./../../assets/images/portfolio2.png')}
                   alt="Project Appointment"/>
               </div>
+              <Modal
+                id="video-modal"
+                show={videoModalActive2}
+                handleClose={closeModal2}
+                children={"<h1>casa</h1>"}
+                videoTag="iframe"
+                titleContent="Weather App"
+                companyContent="Microverse"
+                roleContent="Full Stack Developer"
+                yearContent="2020"
+                projectContent="This project is about building a web application to show a weather forecast using weather API from external services like https://openweathermap.org/ and https://giphy.com/, you should be able to search for a specific location and toggle displaying the data in Fahrenheit or Celsius, to use the app place a name of the city and click on search, every time you click on search the info will go to the left, you can have three different results."
+                featureImage="portfolio2.png"
+                techList={[{id: 1 ,name: "Javascript 6"}, {id: 2 ,name:"Webpack 3"}, {id: 3 ,name:"Rest API"}]}
+                linkGithub={"https://github.com/rubenpazch/js-weather-app"}
+                linkLivePreview={"http://weather-app.cuscosolutions.com/"}
+              />  
             </div>
 
           </div>
